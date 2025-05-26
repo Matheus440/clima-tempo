92d270cac42a45b4b6adfaa2b046496541813241
Matheus440 <menezesmatheus440@gmail.com>
2025-05-26 17:10:15 -0300

    Ajustes no JavaScript

--- js/script.js
+++ js/script.js
@@ -5,16 +5,37 @@ const apiCountryURL = "https://flagsapi.com//flat/64.png"
 const cidadeInput = document.querySelector("#cidade-input")
 const btnBusca = document.querySelector("#buscar")
 
+const cidadeElemento = document.querySelector("#cidade")
+const tempElemento = document.querySelector(".tempet span")
+const bandeiraElemento = document.querySelector("#bandeira")
+const climaElemento = document.querySelector("#clima")
+const climaIconElemento = document.querySelector("#clima-icon")
+const umidadeElemento = document.querySelector("#umidade span")
+const ventoElemento = document.querySelector("#vento span")
+
+
 //Eventos
 btnBusca.addEventListener("click", (e) => {
     e.preventDefault()
+
     const cidade = cidadeInput.value
+
     mostrarDadosPrev(cidade)
 })
 
 
 
 //Funções
+const dadosPrev = async(cidade) => {
+    const urlWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br `
+
+    const res = await fetch(urlWeatherApi)
+    const dados = await res.json()
+
+    console.log(dados)
+
+}
+
 const mostrarDadosPrev = (cidade) => {
-    console.log(cidade)
+    dadosPrev(cidade)
 }
